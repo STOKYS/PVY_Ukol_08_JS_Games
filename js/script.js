638ebd5606ed298d65b2867407447113c4445874
STOKYS <57591015+STOKYS@users.noreply.github.com>
2021-01-18 10:46:29 +0100

    Update v21.0118:11
    Up to date

--- js/script.js
+++ js/script.js
@@ -18,20 +18,51 @@ document.getElementsByTagName("BUTTON")[3].addEventListener("click", function(){
 
 
 
+
 document.getElementsByTagName("BUTTON")[4].addEventListener("click", function(){
-    window.open("https://stokys.github.io/Billy_The_Tax_Evader/",'_blank');
+    window.open("https://stokys.github.io/snake/",'_blank');
 })
 
 document.getElementsByTagName("BUTTON")[5].addEventListener("click", function(){
+    window.open("https://github.com/STOKYS/snake",'_blank');
+})
+
+
+
+
+document.getElementsByTagName("BUTTON")[6].addEventListener("click", function(){
+    window.open("https://stokys.github.io/Billy_The_Tax_Evader/",'_blank');
+})
+
+document.getElementsByTagName("BUTTON")[7].addEventListener("click", function(){
     window.open("https://github.com/STOKYS/Billy_The_Tax_Evader",'_blank');
 })
 
 
 
-document.getElementsByTagName("BUTTON")[6].addEventListener("click", function(){
+document.getElementsByTagName("BUTTON")[8].addEventListener("click", function(){
     window.open("https://stokys.github.io/JavaScriptGame/",'_blank');
 })
 
-document.getElementsByTagName("BUTTON")[7].addEventListener("click", function(){
+document.getElementsByTagName("BUTTON")[9].addEventListener("click", function(){
     window.open("https://github.com/STOKYS/JavaScriptGame",'_blank');
+})
+
+
+
+// upcoming
+document.getElementsByTagName("BUTTON")[10].addEventListener("click", function(){
+    window.open("https://github.com/STOKYS/Poker",'_blank');
+})
+
+
+
+document.getElementsByTagName("BUTTON")[11].addEventListener("click", function(){
+    window.open("https://github.com/STOKYS/tower_defece",'_blank');
+})
+
+
+
+document.getElementsByTagName("BUTTON")[12].addEventListener("click", function(){
+    window.open("https://github.com/STOKYS/city_builder",'_blank');
 })
